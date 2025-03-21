7da0e5d7dd71bf79c0a377634dfdfb3490daebea
SandyHeka <hekasandyvr46@gmail.com>
2025-03-21 15:46:19 +1100

    chart added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Sandesh'};
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'GUEST'}
+            subtext="Access and manage your account and transaction"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+
+      </div>
+    </section>
   )
 }
 
